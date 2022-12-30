8296ab9e8b47352a9f906edf32b93553e74ba453
Ke Zhou <104931107+kzhou24@users.noreply.github.com>
2022-12-29 19:11:41 -0800

    Add files via upload

---
+++ WatchMovie/Main.java
@@ -0,0 +1,10 @@
+package WacthMovie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Movie movie = new Adventure("First Blood");
+
+        movie.watchMovie();
+    }
+}
